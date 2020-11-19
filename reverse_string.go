a8100a890acd9d21a09d591b1df26651eaa4ed21
jayaraman33 <70377145+jayaraman33@users.noreply.github.com>
2020-11-19 16:35:15 +0530

    first commit

---
+++ reverse_string.go
@@ -0,0 +1,11 @@
+package reverse
+
+func Reverse(str string) string {
+	chars := []rune(str)
+
+	for i, j := 0, len(chars)-1; i < j; i, j = i+1, j-1 {
+		chars[i], chars[j] = chars[j], chars[i]
+	}
+
+	return string(chars)
+}
